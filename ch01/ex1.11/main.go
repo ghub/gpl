fce9d62f968da6f73fad4a02b5f8e7fcb79ab572
Ge Wang <code.gewang@gmail.com>
2019-05-08 18:41:47 -0600

    Finished section 1.6

---
+++ ch01/ex1.11/main.go
@@ -0,0 +1,8 @@
+// Exercise 1.11: Try fetchall with longer argument lists, such as samples from
+// the top million web sites available at alexa.com. How does the program
+// behave if a web site just doesn’t respond? (Section 8.9 describes mechanisms
+// for coping in such cases.)
+
+// Answer:
+// If block the web site, will see
+// read: connection reset by peer
